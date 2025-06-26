9669b74cce255d43529f505cf710f27ff6702394
Denis Cooper <denis.cooper09@gmail.com>
2025-06-26 12:47:43 +0100

    [TurboProjectile] feat: shrink vector table to 8 bytes
    Only SP and reset handler are stored in the table now.

--- TurboProjectile/src/startup.c
+++ TurboProjectile/src/startup.c
@@ -1,10 +1,13 @@
 #include "types.h"
+#include "interrupts.h"
 
 extern void main(void);
 extern void _estack(void);
 
 __attribute__((naked, noreturn)) void _reset(void)
 {
+    cpsid();
+    
     extern u32 _sbss, _ebss, _sdata, _edata, _sidata;
     
     for (u32 *dst = &_sbss; dst < &_ebss; ++dst) *dst = 0UL;
@@ -15,7 +18,7 @@ __attribute__((naked, noreturn)) void _reset(void)
     while (1) (void) 0;
 }
 
-__attribute__((section(".vectors"))) void (*const vectors[16 + 85 + 1])(void) = 
+__attribute__((section(".vectors"))) void (*const vectors[2])(void) = 
 {
     _estack,
     _reset
