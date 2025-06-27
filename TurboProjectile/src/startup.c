c0946b42d4c3971344fab730fbd36d58e80dbe2c
Denis Cooper <denis.cooper09@gmail.com>
2025-06-27 11:22:11 +0100

    [TuboProjectile] feat(96MHz): enable 3WS for flash

--- TurboProjectile/src/startup.c
+++ TurboProjectile/src/startup.c
@@ -1,13 +1,21 @@
-#include "types.h"
+#include "flash.h"
 #include "interrupts.h"
 
 extern void main(void);
 extern void _estack(void);
 
+static inline void _init_flash()
+{
+    FLASH->ACR = 3UL | (FLASH->ACR & ~3UL);                 // 3WS (96MHz SYSCLK)
+    FLASH->ACR |= (7UL << 8);                               // Enable data cache, instruction cache and prefetch
+}
+
 __attribute__((naked, noreturn)) void _reset(void)
 {
     cpsid();
     
+    _init_flash();
+ 
     extern u32 _sbss, _ebss, _sdata, _edata, _sidata;
     
     for (u32 *dst = &_sbss; dst < &_ebss; ++dst) *dst = 0UL;
