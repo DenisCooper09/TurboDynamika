a13ff482fd948201cd15fa7730ecf583a9deafab
Denis Cooper <denis.cooper09@gmail.com>
2025-06-27 11:51:56 +0100

    [TurboProjectile] refactor: move initialization from _reset function into smaller functions

--- TurboProjectile/src/startup.c
+++ TurboProjectile/src/startup.c
@@ -4,6 +4,7 @@
 
 extern void main(void);
 extern void _estack(void);
+void _reset(void);
 
 static inline void _init_flash()
 {
@@ -29,18 +30,16 @@ static inline void _init_clock()                                    // 96MHz SYS
     while ((RCC->CFGR & (3UL << 2)) != (2UL << 2));
 }
 
-__attribute__((naked, noreturn)) void _reset(void)
+static inline void _init_memories()
 {
-    cpsid();
-    
-    _init_flash();
-    _init_clock();
- 
     extern u32 _sbss, _ebss, _sdata, _edata, _sidata;
     
     for (u32 *dst = &_sbss; dst < &_ebss; ++dst) *dst = 0UL;
     for (u32 *dst = &_sdata, *src = &_sidata; dst < &_edata;) *dst++ = *src++;
+}
 
+static inline void _init_vector_table()
+{
     extern void (*_svectors[])(void), (*_evectors[])(void);
     
     for (void (**dst)(void) = _svectors; dst < _evectors; ++dst) *dst = default_isr;
@@ -48,7 +47,17 @@ __attribute__((naked, noreturn)) void _reset(void)
     _svectors[0] = _estack;
     _svectors[1] = _reset;
     
-    *((volatile u32 *)(0xE000ED08)) = (u32)_svectors;
+    *((volatile u32 *)(0xE000ED08)) = (u32)_svectors;               // Set vector table offset (VTOR)
+}
+
+__attribute__((naked, noreturn)) void _reset(void)
+{
+    cpsid();
+    
+    _init_flash();
+    _init_clock();
+    _init_memories();
+    _init_vector_table();
     
     cpsie();
     
